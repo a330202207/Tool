359e6b35279af6b6c964a8914d480ca4d7c699f3
renminjie <330202207@qq.com>
2018-04-09 10:57:35 +0800

    add

--- StringFunctions.php
+++ StringFunctions.php
@@ -1,2 +1,18 @@
 <?php
 
+/**
+ * @notes: 将数组分割拼接成,形式字符串
+ * @author: KevinRen<330202207@qq.com>
+ * @date: 2018/4/8
+ * @param array $arr
+ * @param $valName
+ * @return string
+ * $arr = [[id => '1'], [id => '2'], [id => '3']];
+ * return '1,2,3'
+ */
+function spliceStr(array $arr, $valName)
+{
+    $data = array_column($arr, $valName);
+    $str = implode(',', $data);
+    return $str;
+}
