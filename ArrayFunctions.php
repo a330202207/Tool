921bc2309d46d4b6df197ca334f99dd195ac949c
renminjie <330202207@qq.com>
2018-04-09 10:55:15 +0800

    add

--- ArrayFunctions.php
+++ ArrayFunctions.php
@@ -1,2 +1,19 @@
 <?php
 
+/**
+ * 多维数组去重
+ * @param array
+ * @return array
+ */
+function super_unique($array)
+{
+    $result = array_map("unserialize", array_unique(array_map("serialize", $array)));
+
+    foreach ($result as $key => $value) {
+        if (is_array($value)) {
+            $result[$key] = super_unique($value);
+        }
+    }
+
+    return $result;
+}
--- FileFunctions.php
+++ FileFunctions.php
@@ -1,2 +1,56 @@
 <?php
 
+/**
+ * @notes: 导出excel(csv)
+ * @author: KevinRen<330202207@qq.com>
+ * @date: 2018/4/9
+ * @param array $data 导出数据
+ * @param array $headlist 第一行,列名
+ * @param $fileName 输出Excel文件名
+ */
+function csv_export(array $data = [], array $headlist = [], $fileName)
+{
+
+    header('Content-Type: application/vnd.ms-excel');
+    header('Content-Disposition: attachment;filename="' . $fileName . '.csv"');
+    header('Cache-Control: max-age=0');
+
+    //打开PHP文件句柄,php://output 表示直接输出到浏览器
+    $fp = fopen('php://output', 'a');
+
+    //输出Excel列名信息
+    foreach ($headlist as $key => $value) {
+        //CSV的Excel支持GBK编码，一定要转换，否则乱码
+        $headlist[$key] = iconv('utf-8', 'gbk', $value);
+    }
+
+    //将数据通过fputcsv写到文件句柄
+    fputcsv($fp, $headlist);
+
+    //计数器
+    $num = 0;
+
+    //每隔$limit行，刷新一下输出buffer，不要太大，也不要太小
+    $limit = 100000;
+
+    //逐行取出数据，不浪费内存
+    $count = count($data);
+    for ($i = 0; $i < $count; $i++) {
+
+        $num++;
+
+        //刷新一下输出buffer，防止由于数据过多造成问题
+        if ($limit == $num) {
+            ob_flush();
+            flush();
+            $num = 0;
+        }
+
+        $row = $data[$i];
+        foreach ($row as $key => $value) {
+            $row[$key] = iconv('utf-8', 'gbk', $value);
+        }
+
+        fputcsv($fp, $row);
+    }
+}
