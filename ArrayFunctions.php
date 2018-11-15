670f8f88a729c92d305e3c28c9a1caa1ee5f7635
KevinRen <330202207@qq.com>
2018-11-15 11:42:41 +0800

    add

--- ArrayFunctions.php
+++ ArrayFunctions.php
@@ -17,3 +17,31 @@ function super_unique($array)
 
     return $result;
 }
+
+/**
+ * 多维数组多个字段排序
+ * @param array
+ * @return array
+ * sortArrByManyField($arr, 'id', SORT_ASC, 'name', SORT_ASC, 'age', SORT_DESC);
+ */
+function sortArrByManyField()
+{
+    $args = func_get_args();
+    if (empty($args)) {
+        return null;
+    }
+    $arr = array_shift($args);
+    foreach ($args as $key => $value) {
+        if (is_string($value)) {
+            $temp = [];
+            foreach ($arr as $k => $v) {
+                $temp[$k] = $v[$value];
+            }
+            $args[$key] = $temp;
+        }
+    }
+    //引用值
+    $args[] = &$arr;
+    call_user_func_array('array_multisort', $args);
+    return array_pop($args);
+}
